a8751cc88795ac379b2003c3f8e0d3c1c36bb97c
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-01-09 22:20:09 +0800

    Finished DOM structure

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 "use strict";   // Always use strict mode
+function checkMatchStatus () {
+
+}
+
+// Resets the board by clearing the cells and adding
+function resetGame () {
+    board.innerHTML = ""; // Removes the children of "board" element
+    isPlayer1 = true; // Resets the variable to have player 1 move first
+
+    // Generates board cell and inserts it as a child element of board-container
+    for (let i = 0; i < 9; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("click", cellCheck);
+        board.appendChild(cell);
+    }
+}
+
+// Checks cell clicked if its already checked
+// if not, checks the cell
+function cellCheck (e) {
+    let target = e.currentTarget;
+    if (target.hasChildNodes() === false) {
+        let check = document.createElement("div");
+        check.className = isPlayer1 ? "p1Check" : "p2Check" ;
+        target.appendChild(check);
+        isPlayer1 = !isPlayer1;
+    }
+}
+
+let isPlayer1 = true ; // Used for knowing whether player 1 or player 2 is the one making a move
 
 // Gets the board-container element from the DOM
 let board = document.getElementById("board");
 
-// Generates board cell and inserts it as a child element of board-container
-for (let i = 0; i < 9; i++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    board.appendChild(cell);
-}
+resetGame(); // initial setup
 
-// Puts the cells in array "cells"
-let cells = Array.from(board.childNodes);
-console.log(cells.length);
+let cells = Array.from(board.children); // Puts the cells in array "cells"
+console.log(cells[0]);
 
-// Labels each cell their respective cell number
-for (let i = 0; i < cells.length; i++) {
-    console.log(i);
-    cells[i].innerHTML = i;
-}
